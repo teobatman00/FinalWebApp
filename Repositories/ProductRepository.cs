5af489c07a2f760a7a61c0ce9ee6215523ee9960
Minh Duong <teobatman00@gmail.com>
2022-06-05 20:59:36 +0700

    update new change

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using FinalWebApp.Context;
+using FinalWebApp.Entities;
+using FinalWebApp.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalWebApp.Repositories
+{
+    public class ProductRepository : BaseRepository<ProductRepository, ProductEntity, string>, IProductRepository
+    {
+        public ProductRepository(ILogger<ProductRepository> logger, ApplicationContext context) : base(logger, context)
+        {
+        }
+    }
+}
